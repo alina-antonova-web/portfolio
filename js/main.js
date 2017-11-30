943b1994aa36c2f4f492e62501cbbe79d32d39f6
alina3agit <antonova.alina@gmail.com>
2017-11-30 14:31:14 +0100

    Added Fancybox

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
-	
+	// fancybox
+
+	$(".fancybox").fancybox({
+		closeBtn: true,
+		arrows : true,
+		nextClick: true,
+
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
 	
 });
