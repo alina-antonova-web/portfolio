e4b2c3719a72c7c5ef187c99e2f893b132335b74
alina3agit <antonova.alina@gmail.com>
2017-12-06 19:17:00 +0100

    Edeted Google Form

--- js/validate_form.js
+++ js/validate_form.js
@@ -2,20 +2,22 @@ $(document).ready(function() {
 
 	var submitted=false;
 
+	$('#hidden_iframe').load(function() {if(submitted) {}});
+
 	$('#contact-form').validate({
 		rules: {
-			entry.1468846692: { required: true },
-			entry.29343622: { required: true, email: true },
-			entry.1953925097: { required: true }
+			"entry.1468846692": { required: true },
+			"entry.29343622": { required: true, email: true },
+			"entry.1953925097": { required: true }
 		},
 
 		messages: {
-			entry.1468846692: "Пожалуйста, введите своё имя.",
-			entry.29343622: {
+			"entry.1468846692": "Пожалуйста, введите своё имя.",
+			"entry.29343622": {
 				required: "Пожалуйста, введите свой email.",
 				email: "Email должен быть в формате name@domaim.com. Возможно Вы ввели email с ошибкой."
 			},
-			entry.1953925097: "Пожалуйста, введите текст сообщения.",
+			"entry.1953925097": "Пожалуйста, введите текст сообщения.",
 
 		},
 
