b045e701867b48d5cd758cf1d9d9a2ff571467db
alina3agit <antonova.alina@gmail.com>
2017-12-06 20:06:49 +0100

    Ajax to another server

--- js/validate_form.js
+++ js/validate_form.js
@@ -1,29 +1,24 @@
 $(document).ready(function() {
 
-	var submitted=false;
-
-	$('#hidden_iframe').load(function() {if(submitted) {}});
-
 	$('#contact-form').validate({
 		rules: {
-			"entry.1468846692": { required: true },
-			"entry.29343622": { required: true, email: true },
-			"entry.1953925097": { required: true }
+			fio: { required: true },
+			email: { required: true, email: true },
+			user_message: { required: true }
 		},
 
 		messages: {
-			"entry.1468846692": "Пожалуйста, введите своё имя.",
-			"entry.29343622": {
+			fio: "Пожалуйста, введите своё имя.",
+			email: {
 				required: "Пожалуйста, введите свой email.",
 				email: "Email должен быть в формате name@domaim.com. Возможно Вы ввели email с ошибкой."
 			},
-			"entry.1953925097": "Пожалуйста, введите текст сообщения.",
+			user_message: "Пожалуйста, введите текст сообщения.",
 
 		},
 
 		submitHandler: function() {
-			//ajaxFormSubmit();
-			gForm();
+			ajaxFormSubmit();
 		}
 	});
 
@@ -36,7 +31,7 @@ $(document).ready(function() {
 
 		$.ajax({
 			type: "POST",
-			url: "php/mail.php",
+			url: "https://alina-web.000webhostapp.com/php/mail.php",
 			data: string,
 
 			success: function(html){
@@ -46,14 +41,6 @@ $(document).ready(function() {
 		});
 
 		return false;
-	}
-
-
-	// Google form
-
-	function gForm() {
-		$("#contact-form").slideUp(800);
-		$('#answer').html("<div class='contact-form__success'>Форма отправлена успешно!</div>");
 	}
 
 });
